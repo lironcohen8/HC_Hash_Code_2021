ef4c1c21fd402adf495132496dd5fcd1bd0a1a03
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-03-06 22:13:46 +0200

    new amazing formula

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,7 @@ public class Main {
 				bufferedWriter.write(tempIntersection.getPositiveStreets()+ "\n");
 				for (Street street : tempIntersection.incomingStreet) {
 					if (street.priorityValue>0) {
-						int factor = (int) Math.ceil((street.priorityValue/tempIntersection.priorityInt)*ReadInput.D);
+						int factor = (int) Math.ceil(((double)street.priorityValue/tempIntersection.priorityInt)*ReadInput.D/1000);
 						bufferedWriter.write(street.name+ " " + factor +"\n");
 					}
 				}
